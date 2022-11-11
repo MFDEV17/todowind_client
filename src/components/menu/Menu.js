c68899109d7c2ab4b08f39a8dd38cca6e6b61d5e
strahsvid17 <maximsushkin02@gmail.com>
2022-11-11 15:21:58 +0400

    Little changes of menu styling

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,12 +2,13 @@ import { HiOutlineInboxArrowDown } from "react-icons/hi2";
 import { BsCloudSun } from "react-icons/bs";
 import { AiOutlineCalendar } from "react-icons/ai";
 import ProjectList from "./ProjectList";
+import FavoriteList from "./FavoriteList";
 
 const Menu = () => {
   return (
     <div className="drawer-side">
-      <label htmlFor="my-drawer" className="drawer-overlay"/>
-      <ul className="px-2 py-3 w-80 bg-base-100 text-base-content">
+      <label htmlFor="my-drawer" className="drawer-overlay" />
+      <ul className="px-2 w-80 bg-base-100 text-base-content py-10">
         <li className="hidden max-[1000px]:block">
           <input
             type="text"
@@ -36,6 +37,7 @@ const Menu = () => {
           </div>
           <span>34</span>
         </li>
+        <FavoriteList list={[1, 1, 1, 1, 1]} />
         <ProjectList
           list={[
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2,
