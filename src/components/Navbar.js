2e37488f6b8f692a460237bcbd2b188f02c3d7f5
strahsvid17 <maximsushkin02@gmail.com>
2022-11-15 00:26:32 +0400

    Changed label id and added responsive size for an icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,8 @@ const Navbar = () => {
     <div className="flex justify-between items-center mt-5 border-b-gray-600 max-w-full px-4 border-b pb-4">
       <div className="flex gap-5 items-center text-2xl">
         <div className="tooltip tooltip-bottom" data-tip="Menu [m]">
-          <label className="btn btn-ghost btn-square" htmlFor="my-drawer">
-            <HiMenuAlt2 className="h-7 w-7" />
+          <label className="btn btn-ghost btn-square" htmlFor="menu-drawer">
+            <HiMenuAlt2 className="h-7 w-7 max-[420px]:w-6 max-[420px]:h-6" />
           </label>
         </div>
         <span className="font-bold max-[500px]:hidden">Todowind</span>{" "}
@@ -33,23 +33,23 @@ const Navbar = () => {
       <div className="btn-group">
         <div className="tooltip tooltip-bottom" data-tip="Add task [q]">
           <div className="btn btn-ghost normal-case">
-            <AiOutlinePlus className="h-7 w-7" />
+            <AiOutlinePlus className="h-7 w-7 max-[420px]:w-6 max-[420px]:h-6" />
           </div>
         </div>
         <div className="tooltip tooltip-bottom" data-tip="Your productivity">
           <div className="btn btn-ghost normal-case gap-3">
-            <GiStairsGoal className="h-7 w-7" />
+            <GiStairsGoal className="h-7 w-7 max-[420px]:w-6 max-[420px]:h-6" />
             <span className="max-[690px]:hidden">12 / 25</span>
           </div>
         </div>
         <div className="tooltip tooltip-bottom" data-tip="Notifications">
           <div className="btn btn-ghost normal-case">
-            <MdNotificationsNone className="h-7 w-7" />
+            <MdNotificationsNone className="h-7 w-7 max-[420px]:w-6 max-[420px]:h-6" />
           </div>
         </div>
         <div className="tooltip tooltip-bottom" data-tip="Account">
           <div className="btn btn-ghost normal-case">
-            <MdOutlineAccountCircle className="h-7 w-7" />
+            <MdOutlineAccountCircle className="h-7 w-7 max-[420px]:w-6 max-[420px]:h-6" />
           </div>
         </div>
       </div>
